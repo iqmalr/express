99a24e00080696f00033614c2bae76cb099d6bd6
Iqmal Riffai <m.iqmal.riffai@gmail.com>
2025-09-15 21:07:08 +0700

    add health check

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
 const welcomeStrings = [
   "Hello Express!",
   "To learn more about Express on Vercel, visit https://vercel.com/docs/frameworks/backend/express",
-]
+];
 
-app.get('/', (_req, res) => {
-  res.send(welcomeStrings.join('\n\n'))
-})
-
-export default app
+app.get("/", (_req, res) => {
+  res.send(welcomeStrings.join("\n\n"));
+});
+app.get("/api/health", (_req, res) => {
+  res.json({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+  });
+});
+export default app;
